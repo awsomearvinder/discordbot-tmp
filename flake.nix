dc2c2d18319044e81922658bc84ba4e98dd09aac
awesomearvinder <ArvinderDhan@gmail.com>
2021-10-17 04:10:05 -0500

    Add typescript-language-server and set $PATH. Also format
    Yes. This is a bad commit. does way too much. It's fine probably.

--- flake.nix
+++ flake.nix
@@ -4,18 +4,20 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
-  outputs = { self, nixpkgs, flake-utils }: 
-  flake-utils.lib.eachSystem 
-    ["x86_64-linux"] 
-    (system: 
-      let pkgs = nixpkgs.legacyPackages.${system}; 
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
       in {
         devShell = (pkgs.mkShell {
-          nativeBuildInputs = [ pkgs.nodejs ]; 
-          /* We want to use the npm lock-file as much as possible. 
-          Not everyone uses nix. Everyone uses npm. */
-          buildInputs = [];
+          nativeBuildInputs =
+            [ pkgs.nodejs-16_x pkgs.nodePackages.typescript-language-server ];
+          /* We want to use the npm lock-file as much as possible.
+             Not everyone uses nix. Everyone uses npm.
+          */
+          buildInputs = [ ];
+          shellHook = ''
+            export PATH=$PATH:./node_modules/.bin
+          '';
         });
-      }
-    );
+      });
 }
