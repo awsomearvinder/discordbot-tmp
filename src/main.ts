db5cd16de98ed43b6f96d156c3ff10ed3e020e9f
Arvinder <ArvinderDhan@gmail.com>
2022-01-15 00:05:58 -0600

    Finish the repo setup.

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,9 @@
-console.info("test");
+import { Client, Message, ClientOptions, Intents} from "discord.js";
+
+let client = new Client({intents: [Intents.FLAGS.DIRECT_MESSAGES, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MEMBERS]});
+
+client.login("token");
+
+client.on("message", async (msg) => {
+    await msg.reply("test");
+})
